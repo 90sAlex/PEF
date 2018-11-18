bac690e47d8f2bc6d6a47f2df75de35168533773
90sAlex <44882821+90sAlex@users.noreply.github.com>
2018-11-18 12:34:49 -0600

    Add files via upload

--- admin.php
+++ admin.php
@@ -4,11 +4,11 @@
 if(session_id() == '' || !isset($_SESSION)){session_start();}
 
 if(!isset($_SESSION["username"])) {
-  header("location:inicio.php");
+  header("location:index.php");
 }
 
 if($_SESSION["type"]!="admin") {
-  header("location:inicio.php");
+  header("location:index.php");
 }
 
 include 'config.php';
@@ -85,16 +85,17 @@ include 'config.php';
                           <span class="icon-bar"></span>
                           <span class="icon-bar"></span>
                       </button>
-                      <a class="navbar-brand" href="index.html"><img class="img-responsive" src="images/logo.png" alt="Logo"></a>
+                      <a class="navbar-brand" href="index.php"><img class="img-responsive" src="images/logo.png" alt="Logo"></a>
                   </div>
                   <div class="collapse navbar-collapse" id="navbar-collapse">
                       <ul class="nav navbar-nav navbar-right">
-                          <li class="active"><a href="inicio.php">Inicio</a></li>
+                          <li class="active"><a href="index.php">Inicio</a></li>
                           <li><a href="about.php">Quiénes Somos</a></li>
                           <li><a href="products.php">Productos</a></li>
                           <li><a href="cart.php">Carrito</a></li>
                           <li><a href="orders.php">Ordenes</a></li>
                           <li><a href="contact.php">Contacto</a></li>
+                          <li><a href="estadisticas.php">Estadisticas</a></li>
                           <?php
 
                           if(isset($_SESSION['username'])){
@@ -113,43 +114,33 @@ include 'config.php';
       </div><!-- main menu -->
   </header><!-- Header -->
 
-  <div class="breadcrumb-section image-bg ">
-	  <div class="overlay"></div>
-	  <div class="breadcrumb-content container">
-		  <h1>Mi cuenta</h1>
-		  <ol class="breadcrumb">
-			  <li><a href="inicio.php">Regresar</a></li>
-			  <li class="active">Account</li>
-		  </ol>
-	  </div>
-  </div><!-- breadcrumb-section -->
+    <div class="row" style="margin-top:10px;">
+      <div class="large-12">
+        <h3>Hey Administrador!</h3>
+        <?php
+          $result = $mysqli->query("SELECT * from products order by id asc");
+          if($result) {
+            while($obj = $result->fetch_object()) {
+              echo '<div class="large-4 columns">';
+              echo '<p><h3>'.$obj->product_name.'</h3></p>';
+              echo '<img src="images/products/'.$obj->product_img_name.'"/>';
+              echo '<p><strong>Codigo de producto</strong>: '.$obj->product_code.'</p>';
+              echo '<p><strong>Descripcion</strong>: '.$obj->product_desc.'</p>';
+              echo '<p><strong>Unidades disponibles</strong>: '.$obj->qty.'</p>';
+              echo '<div class="large-6 columns" style="padding-left:0;">';
+              echo '<form method="post" name="update-quantity" action="admin-update.php">';
+              echo '<p><strong>Agregar</strong>:</p>';
+              echo '</div>';
+              echo '<div class="large-6 columns">';
+              echo '<input type="number" name="quantity[]"/>';
 
-
-
-  <!--
-      <img data-interchange="[images/bolt-retina.jpg, (retina)], [images/bolt-landscape.jpg, (large)], [images/bolt-mobile.jpg, (mobile)], [images/bolt-landscape.jpg, (medium)]">
-      <noscript><img src="images/bolt-landscape.jpg"></noscript>
-  -->
-
-	  <div class="container">
-		  <div class="section-title text-center">
-			  <h1>Adminstrador</h1>
-		  </div>
-       <div class="section-padding">
-		  <button  onclick="window.location.replace('index.php/examples/users_management')"
-				   type="button">Configuración de Usuarios</button>
-
-		  <button  onclick="window.location.replace('index.php/examples/orders_management')"
-				   type="button">Configuración de Ordenes</button>
-
-		  <button  onclick="window.location.replace('index.php/examples/products_management')"
-				   type="button">Configuración de Productos</button>
-	   </div>
-
-	  </div><!-- container -->
-
-
-
+              echo '</div>';
+              echo '</div>';
+            }
+          }
+        ?>
+      </div>
+    </div>
 <footer class="footer">
       <div class="contact-section">
           <div class="container">
